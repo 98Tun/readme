8b5598ed995fbca70e75bb4dd69999b6316b1b00
Vũ Ngọc Tuấn <vaicadogbk05@gmail.com>
2026-03-18 23:23:32 +0700

    Update RC4.cpp

--- RC4.cpp
+++ RC4.cpp
@@ -1,1 +1,24 @@
+def ksa(s_arr, key_arr):
+    s = list(s_arr)
+    j = 0
+    n = len(s)
+    
+    for i in range(n):
+        j = (j + s[i] + key_arr[i % len(key_arr)]) % n
+        s[i], s[j] = s[j], s[i]
+        
+    return s
 
+def prga(s_arr):
+
+    i, j = 0, 0
+    n = len(s_arr)
+    
+    while True:
+        i = (i + 1) % n
+        j = (j + s_arr[i]) % n
+        
+        s_arr[i], s_arr[j] = s_arr[j], s_arr[i]
+        
+        t = (s_arr[i] + s_arr[j]) % n
+        yield s_arr[t]
